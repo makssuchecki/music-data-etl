fd35ddc920ef64b6111d0ec49556755d037abea9
makssuchecki <maks.suchecki@gmail.com>
2026-08-23 20:35:00 +0200

    chore: cleaning

--- ingestion/lastfm/models.py
+++ ingestion/lastfm/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from typing import Optional
+
 from pydantic import BaseModel, field_validator
+
 
 class Scrobble(BaseModel):
     track_name: str
--- ingestion/loader.py
+++ ingestion/loader.py
@@ -1,7 +1,9 @@
 import os
+
 import psycopg2
-from psycopg2.extras import execute_values
 from dotenv import load_dotenv
+from psycopg2.extras import execute_values
+
 from ingestion.lastfm.models import Scrobble
 
 load_dotenv()
